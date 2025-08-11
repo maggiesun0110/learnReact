bc8431c7d997f6bd6a5fbc18287a8e32d565671a
Maggie <maggiesun625@gmail.com>
2025-08-11 16:01:29 -0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function Header(){
+  let title = "first component";
+  return <h1>{title}</h1>
+}
+
+function App() {
+  return <Header/>
+}
+
+export default App;
