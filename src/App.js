c1135ac37edb221be95c24d3eb0eba91834a3522
Maggie <maggiesun625@gmail.com>
2025-08-18 16:37:07 -0700

    finishing updating changes

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Intro1 from './components/Intro1';
 import Intro2 from './components/Intro2';
 import Intro3 from './components/Intro3';
 import Nav from './components/Nav';
+import Btn from "./components/Btn";
 
 
 
@@ -16,10 +17,12 @@ function App() {
       <Intro1 color = "purple"/>
       <Intro2 />
       <Intro3 />
+      <Btn text = "click me"/>
+      <Btn text = "no, click me!"/>
       <Footer />
       <p>hi</p>
       <p>whoa i can repeat so easily look another intro1 component. this is like using functions with html</p>
-      <Intro1 />
+      <Intro1 color = "midnightblue"/>
     </>
   );
 }
